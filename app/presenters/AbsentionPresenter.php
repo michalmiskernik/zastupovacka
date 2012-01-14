3d296c3e5d0895b117c7186875d3be30295b4fd4
Michal Miškerník <michal.miskernik@gmail.com>
2012-01-14 22:29:14 +0100

    reporting absentions

---
+++ app/presenters/AbsentionPresenter.php
@@ -0,0 +1,52 @@
+<?php
+
+use Nette\Application\UI;
+
+class AbsentionPresenter extends BasePresenter
+{
+  /** @permission(absention, report) */
+  public function renderReport()
+  {
+    
+  }
+
+  public function processReportForm(UI\Form $form)
+  {
+    $values = $form->getValues();
+
+    $absention = array(
+      'date' => new \DateTime($values['date']),
+      'teacher_id' => $this->context->user->identity->data['id']
+    );
+
+    $hours = 0;
+    for ($i = 0; $i <= 8; $i++) {
+      if ($values['hour_' . $i]) {
+        $hours += pow(2, $i);
+      }
+    }
+    $absention['hours'] = $hours;
+
+    $this->context->database->exec('INSERT INTO absentions', $absention);
+    
+    $this->redirect('default');
+  }
+
+  protected function createComponentReportForm()
+  {
+    $form = new UI\Form;
+
+    $form->addText('date', 'Dátum')
+      ->setRequired('Prosím zadaj dátum.');
+
+    for ($i = 0; $i <= 8; $i++) {
+      $form->addCheckbox('hour_' . $i);
+    }
+
+    $form->addSubmit('send', 'Nahlásiť');
+
+    $form->onSuccess[] = callback($this, 'processReportForm');
+
+    return $form;
+  }
+}
