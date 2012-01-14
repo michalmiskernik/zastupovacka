4167e679c689b2fff0d33e237788d8ce55f65bc3
Michal Miškerník <michal.miskernik@gmail.com>
2012-01-14 15:53:57 +0100

    added list viewing

--- app/presenters/ListPresenter.php
+++ app/presenters/ListPresenter.php
@@ -4,11 +4,27 @@ class ListPresenter extends BasePresenter
 {
   public function renderDefault()
   {
-    $this->template->lists = $this->table('list')->order('date');
+    $this->template->lists = $this->table('lists')->order('date');
+  }
+
+  public function renderToday()
+  {
+    $date = new \DateTime('today');
+    $this->loadList($date);
+    $this->setView('view');
   }
 
   public function renderView($date)
   {
-    
+    $date = new \DateTime($date);
+    $this->loadList($date);
+  }
+
+  protected function loadList(\DateTime $date)
+  {
+    $list = $this->table('lists')->where('date', $date)->fetch();
+    $this->template->list = $list;
+    $this->template->substitutions = $list
+      ->related('substitutions')->order('absention_id');
   }
 }
