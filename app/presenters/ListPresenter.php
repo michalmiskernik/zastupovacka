b7bba2baacf3316cc0ac33ad892147256ca99a99
Michal Miškerník <michal.miskernik@gmail.com>
2012-01-15 17:02:02 +0100

    saving list to session when editing

--- app/presenters/ListPresenter.php
+++ app/presenters/ListPresenter.php
@@ -23,7 +23,7 @@ class ListPresenter extends BasePresenter
 
     if (!$this->list) return;
 
-    $this->copyListToSession();
+    $this->template->substitutions = $this->getListFromSession()->substitutions;
   }
 
   protected function loadList(\DateTime $date)
@@ -36,5 +36,27 @@ class ListPresenter extends BasePresenter
 
     $this->template->substitutions = $this->substitutions = $list
       ->related('substitutions')->order('absention_id');
+  }
+
+  protected function getListFromSession()
+  {
+    $name = 'list-' . $this->date->format('Y-m-d');
+    $session = $this->context->session;
+
+    if (!$session->hasSection($name)) {
+      $section = $session->getSection($name);
+      $section->setExpiration(0); // until the browser is closed
+      $section->date = $this->list->date;
+      $section->substitutions = $this->getArrayFromList($this->list);
+    }
+
+    return $session->getSection($name);
+  }
+
+  private function getArrayFromList($list)
+  {
+    return array_map(function ($s) {
+      return $s->toArray();
+    }, iterator_to_array($list->related('substitutions')));
   }
 }
