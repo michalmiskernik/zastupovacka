de6663997c2bf76318733a88869ec9654e5fc48b
Michal Miskernik <michal.miskernik@gmail.com>
2012-04-04 21:34:03 +0200

    nette 2.0.3

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,9 +15,9 @@ require LIBS_DIR . '/.composer/autoload.php';
 $configurator = new Nette\Config\Configurator;
 
 // Enable Nette Debugger for error visualisation & logging
-$configurator->setProductionMode(
-	$_SERVER['SERVER_NAME'] !== 'localhost' &&
-	php_sapi_name() !== 'cli-server'
+$configurator->setDebugMode(
+	$_SERVER['SERVER_NAME'] === 'localhost' ||
+	php_sapi_name() === 'cli-server'
 );
 $configurator->enableDebugger(__DIR__ . '/../log');
 
@@ -29,7 +29,7 @@ $configurator->createRobotLoader()
 
 // Create Dependency Injection container from config.neon file
 $configurator->addConfig(__DIR__ . '/config/config.neon',
-	$configurator->productionMode ? $configurator::PRODUCTION : $configurator::DEVELOPMENT);
+	$configurator->debugMode ? $configurator::DEVELOPMENT : $configurator::PRODUCTION);
 
 // Setup dibi
 $configurator->onCompile[] = function ($configurator, $compiler) {
